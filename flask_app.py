7c0dad740e7000838a778eb33dd32a54aa38096e
iamchrishodges <i.am.chris.hodgse.27@gmail.com>
2018-08-22 01:26:43 -0400

    Added UUIDs for Secret Key and Friend Code
    Added UUIDs for Secret Key and Friend Code.
    
    Removed the hard-coded public and private keys. Retrieved by friend code and secret key, respectively.

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,10 @@
-from __future__ import print_function  # Only needed for Python 2
+from __future__ import print_function
 from flask import Flask
 from encryptor import RSAEncryptor
 from sql_engine import SQL_Engine
-import sys
+import sys, uuid
 
-my_pass = b'mypassword'
+my_password = b'mypassword'
 
 my_encryptor = RSAEncryptor()
 
@@ -14,6 +14,7 @@ sql_engine.initialize()
 
 app = Flask(__name__)
 
+
 #if there's no config, create one.
 attempts = 0
 while not app.config.from_pyfile('config.cfg', silent=True):
@@ -21,26 +22,20 @@ while not app.config.from_pyfile('config.cfg', silent=True):
         print('could not write config file to system.')
         sys.exit()
     
-    
-    #create_new_identity(self, a_secret_key, a_public_key, a_private_key, a_friend_code, a_name)
     private_key = my_encryptor.load_private_key("rsa_private.pem", None)
-    s_private_key = my_encryptor.serialize_private_key(private_key, my_pass)
+    s_private_key = my_encryptor.serialize_private_key(private_key, my_password)
     s_public_key = my_encryptor.serialize_public_key(my_encryptor.generate_public_key(private_key))
+    a_secret_key = str(uuid.uuid4())
     
-    sql_engine.create_new_identity('222222', s_public_key, s_private_key, 'bba', 'test')
+    sql_engine.create_new_identity(a_secret_key, s_public_key, s_private_key, str(uuid.uuid4()), 'test')
     f = open('config.cfg','a')
     f.write("DEBUG = True \n")
-    f.write("SECRET_KEY = '222222'\n")
-    f.write("FRIEND_KEY = 'bba'\n")
-
+    f.write("SECRET_KEY ='" + a_secret_key + "'\n")
+  
     f.close()
     attempts =+ 1
 
-secret_key = '222222'
-private_key = my_encryptor.deserialize_private_key(sql_engine.get_private_key('222222'), b'mypassword')
-public_key = my_encryptor.deserialize_public_key(sql_engine.get_public_key('bba'))
-
-    
+secret_key = app.config['SECRET_KEY']
 
 from routes import *
 
